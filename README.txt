ffe9d4624344a6adc75d21808c82bc87c7c73c5e
Luke McCormick <luke@kiza.com>
2014-11-27 14:19:59 -0800

    Initial working lukebs Backdrop theme, incorporating Bootstrap.  Every node is a Jumbotron!

---
+++ README.txt
@@ -0,0 +1,11 @@
+ABOUT lukebs
+-----------
+
+lukebs is a backdrop theme, intended to allow people to take advantage of
+Bootstrap from within Backdrop, in the simplest possible way.
+
+Bootstrap is pulled from the CDN with these 3 lines in page.tpl.php:
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
---
+++ page.tpl.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @file
+ * copied from /core/modules/system/templates/page.tpl.php
+
+ * Lots of info about variables and stuff available there, if you're interested.
+ */
+?><!DOCTYPE html><html<?php print $html_attributes; ?>>
+  <head>
+    <?php print $head; ?>
+    <title><?php print $head_title; ?></title>
+    <?php print $styles; ?>
+    <?php print $scripts; ?>
+
+    <!-- start included Bootstrap code -->
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+    <!-- end included Bootstrap code -->
+
+    <!-- Some code from Bootstrap theme -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- End of code from Bootstrap theme -->
+
+
+    <!-- Custom styles for this Bootstrap theme thempage -->
+    <link href="bs-theme/theme.css" rel="stylesheet">
+
+  </head>
+  <body class="<?php print $classes; ?>" <?php print $body_attributes;?>>
+
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="js/bootstrap.min.js"></script>
+
+
+    <div class="container theme-showcase" role="main">
+      <?php print $page_top; ?>
+      <?php print $page; ?>
+      <?php print $page_bottom; ?>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+  </body>
+</html>
